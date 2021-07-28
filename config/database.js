2f5a1330905c1e4b7c3b062d746825ca8e05f169
Guizmoooe <guillaumemarques@live.fr>
2021-07-28 17:12:53 +0200

    mongoose version

--- config/database.js
+++ config/database.js
@@ -2,19 +2,36 @@ module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: "mongoose",
       settings: {
-        client: "mysql",
-        host: "localhost",
-        port: "3306",
-        database: "scheduler_dock_reception",
-        username: "gmarques",
-        password: "Forever586",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
 });
+
+// env("MY_SQL_DB")
+// ? {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "bookshelf",
+//         settings: {
+//           client: "mysql",
+//           host: "localhost",
+//           port: "3306",
+//           database: "scheduler_dock_reception",
+//           username: "gmarques",
+//           password: "Forever586",
+//           filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//         },
+//         options: {
+//           useNullAsDefault: true,
+//         },
+//       },
+//     },
+//   }
+// :
