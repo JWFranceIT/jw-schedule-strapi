8be76e8ebac4dd602561214e431733e676c2af7b
Guizmoooe <42798311+Guizmoooe@users.noreply.github.com>
2021-08-05 12:15:53 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,34 +1,34 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "mysql",
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: "scheduler_dock_reception",
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+//module.exports = ({ env }) => ({
+ // defaultConnection: "default",
+//  connections: {
+  //  default: {
+//      connector: "bookshelf",
+//      settings: {
+//        client: "mysql",
+//        host: env("DATABASE_HOST"),
+//        port: env("DATABASE_PORT"),
+//        database: "scheduler_dock_reception",
+ //       username: env("DATABASE_USERNAME"),
+  //      password: env("DATABASE_PASSWORD"),
+//        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+ //     },
+//      options: {
+//        useNullAsDefault: true,
+//      },
+ //   },
+//  },
+//});
 
 // ATLAS MONGODB CONNECTION
-// defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         uri: env("DATABASE_URI"),
-//       },
-//       options: {
-//         ssl: true,
-//       },
-//     },
-//   },
+ defaultConnection: "default",
+   connections: {
+     default: {
+       connector: "mongoose",
+       settings: {
+         uri: env("DATABASE_URI"),
+       },
+       options: {
+         ssl: true,
+       },
+    },
+   },
