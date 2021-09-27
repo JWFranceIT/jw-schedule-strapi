b544a32204579f88915b05f4f612fbcf7e207f5e
Guizmoooe <42798311+Guizmoooe@users.noreply.github.com>
2021-09-27 15:38:08 +0200

    rollback redirect

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env('PUBLIC_URL', 'https://jw-schedule.heroku.app.com/admin'),
+  //url: env('PUBLIC_URL', 'https://jw-schedule.heroku.app.com/admin'),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "32bc5c0fdc19190df7a77579ed4bbbee"),
