cba3c3f57b4ccacecc44add52abfccd6d60151ad
JWFranceIT <90705906+JWFranceIT@users.noreply.github.com>
2021-10-19 11:03:01 +0200

    Update pauses.js

--- api/pauses/models/pauses.js
+++ api/pauses/models/pauses.js
@@ -21,13 +21,13 @@ module.exports = {
           data.Thursday ? RRule.TH : "",
           data.Friday ? RRule.FR : "",
         ],
-        byhour: [moment.parseZone(data.heure, "HH").hours()],
+        byhour: [moment.parseZone(data.heure, "HH").utc().hours()],
         byminute: [data.minute],
         bysecond: [0],
         count: [50],
         dtstart: dtStart,
       });
-      
+      console.log("hours", rule.byhour)
       const pauseProvider = await strapi
         .query("providers")
         .findOne({ name: "JW PAUSE" });
@@ -39,13 +39,14 @@ module.exports = {
       }
       data.reception_zones.forEach((zone) => {
         rule.all().map(async (entry) => {
+          console.log({entry})
           await strapi.query("schedule").create({
             provider: pauseProvider.id,
             product_order: "JW PAUSE",
             reception_zone: zone,
-            start: moment(entry).isDST() ? moment(entry).toDate() : moment(entry).add(1, "hours").toDate(),
-            end: moment(entry).isDST() ? moment(entry).add(data.duration, "minutes").toDate() : moment(entry).add(1, "hour").add(data.duration, "minutes").toDate(),
-            promise_date: moment().toDate(),
+            start: moment(entry).isDST() ? moment.utc(entry).toDate() : moment.utc(entry).add(1, "hours").toDate(),
+            end: moment(entry).isDST() ? moment.utc(entry).add(data.duration, "minutes").toDate() : moment.utc(entry).add(1, "hour").add(data.duration, "minutes").toDate(),
+            promise_date: moment.utc().toDate(),
           });
         });
       });
