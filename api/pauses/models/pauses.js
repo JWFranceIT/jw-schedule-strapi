9e74048dbb0ddf1b3137d3f8c3186fd673e42dca
Guizmoooe <guillaumemarques@live.fr>
2021-10-29 13:14:52 +0200

    update pause

--- api/pauses/models/pauses.js
+++ api/pauses/models/pauses.js
@@ -1,3 +1,67 @@
+// "use strict";
+// const { RRule } = require("rrule");
+// const moment = require("moment");
+
+// /**
+//  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+//  * to customize this model
+//  */
+
+// module.exports = {
+//   lifecycles: {
+//     // Called after an entry is created
+//     async beforeCreate(data) {
+//       console.log("first", moment(data.horaire,"hh:mm").utc().format("hh"))
+//       const dtStart = data.DateStart ? new Date(data.DateStart) : new Date();
+//       const rule = new RRule({
+//         freq: RRule.DAILY,
+//         byweekday: [
+//           data.Monday ? RRule.MO : "",
+//           data.Tuesday ? RRule.TU : "",
+//           data.Wednesday ? RRule.WE : "",
+//           data.Thursday ? RRule.TH : "",
+//           data.Friday ? RRule.FR : "",
+//         ],
+//         byhour: [moment(data.horaire, "hh:mm").format("hh")],
+//         byminute: [moment(data.horaire, "hh:mm").format("mm")],
+//         bysecond: [0],
+//         count: [5],
+//         dtstart: dtStart,
+//       });
+
+//       const pauseProvider = await strapi
+//         .query("providers")
+//         .findOne({ name: "JW PAUSE" });
+//       if (!pauseProvider) {
+//         throw new Error("Provider  JW PAUSE not exist");
+//       }
+//       if (data.reception_zones.length === 0) {
+//         throw new Error("Reception zone not defined");
+//       }
+//       data.reception_zones.forEach((zone) => {
+//         rule.all().map(async (entry) => {
+//           console.log({ "entry":entry, "start": moment.parseZone(entry).toDate()  });
+//           await strapi.query("schedule").create({
+//             provider: pauseProvider.id,
+//             product_order: "JW PAUSE",
+//             reception_zone: zone,
+//             start: moment(entry).locale('fr').isDST()
+//               ? moment(entry).locale('fr').toDate()
+//               : moment(entry).locale('fr').add(1, "hours").toDate(),
+//             end: moment(entry).locale('fr').isDST()
+//               ? moment(entry).locale('fr').add(data.duration, "minutes").toDate()
+//               : moment(entry).locale('fr')
+//                   .add(1, "hour")
+//                   .add(data.duration, "minutes")
+//                   .toDate(),
+//             promise_date: moment().locale('fr').utc().toDate(),
+//           });
+//         });
+//       });
+//     },
+//   },
+// };
+
 "use strict";
 const { RRule } = require("rrule");
 const moment = require("moment");
@@ -45,22 +109,19 @@ module.exports = {
             provider: pauseProvider.id,
             product_order: "JW PAUSE",
             reception_zone: zone,
-            start: moment(entry).locale('fr').isDST()
-              ? moment(entry).locale('fr').toDate()
-              : moment(entry).locale('fr').add(1, "hours").toDate(),
-            end: moment(entry).locale('fr').isDST()
-              ? moment(entry).locale('fr').add(data.duration, "minutes").toDate()
-              : moment(entry).locale('fr')
+            start: moment(entry).isDST()
+              ? moment(entry).toDate()
+              : moment(entry).add(1, "hours").toDate(),
+            end: moment(entry).isDST()
+              ? moment(entry).add(data.duration, "minutes").toDate()
+              : moment(entry)
                   .add(1, "hour")
                   .add(data.duration, "minutes")
                   .toDate(),
-            // start: moment(entry).utc().toDate(),
-            // end: moment(entry).utc().add(data.duration, 'minutes').toDate(),
-            promise_date: moment().locale('fr').utc().toDate(),
+            promise_date: moment().utc().toDate(),
           });
         });
       });
     },
   },
 };
-
