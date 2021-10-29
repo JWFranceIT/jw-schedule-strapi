897002e8335b6177b4445083612b11938c7de570
JWFranceIT <90705906+JWFranceIT@users.noreply.github.com>
2021-10-29 14:20:38 +0200

    Update pauses.js

--- api/pauses/models/pauses.js
+++ api/pauses/models/pauses.js
@@ -1,3 +1,11 @@
+"use strict";
+const { RRule } = require("rrule");
+const moment = require("moment");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
 
 module.exports = {
   lifecycles: {
